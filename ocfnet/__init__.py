a9e36de5f79b7a77ec683e8478055fbaa5156699
spectralsun <garrett@spectralsun.com>
2015-08-15 21:19:32 -0700

    page and events, catch all route

--- ocfnet/__init__.py
+++ ocfnet/__init__.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 
 # Assets: js, jsx, css
 assets = Environment(app)
-js = Bundle()
 assets.register('js', js)
 jsx = Bundle()
 assets.register('jsx', jsx)
 css = Bundle()
 assets.register('css', css)
 
-@app.route('/')
-def index():
-    return render_templete('index.html')
+@app.route('/<path:path>')
+def index(path):
+    """Main route for the single page app"""
+    return render_template('index.html')
