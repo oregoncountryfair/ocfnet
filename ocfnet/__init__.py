718c249c931533b300745cad7ebeb1c69fdd302f
spectralsun <garrett@spectralsun.com>
2015-08-15 18:28:01 -0700

    assets and index route

--- ocfnet/__init__.py
+++ ocfnet/__init__.py
@@ -1,4 +1,18 @@
-from flask import Flask
+from flask import Flask, render_templete
+from flask.ext.assets import Environment, Bundle
 
 
 app = Flask(__name__)
+
+# Assets: js, jsx, css
+assets = Environment(app)
+js = Bundle()
+assets.register('js', js)
+jsx = Bundle()
+assets.register('jsx', jsx)
+css = Bundle()
+assets.register('css', css)
+
+@app.route('/')
+def index():
+    return render_templete('index.html')
