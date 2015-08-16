52c28701201cdab236870de9dcb2bf3f5535fb87
spectralsun <garrett@spectralsun.com>
2015-08-16 15:40:18 -0700

    secret key and csrf

--- configdist.py
+++ configdist.py
@@ -1,4 +1,6 @@
 DATABASE_URL = 'sqlite:///sqlite.db'
 
+SECRET_KEY = 'this is not secret so you should change it'
+
 UPLOAD_PATH = 'ocfnet/static/uploads/'
 UPLOAD_WEB_PATH = '/static/uplaods/'
--- ocfnet/__init__.py
+++ ocfnet/__init__.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_templete
+from flask import Flask, render_template, session
 from flask.ext.assets import Environment, Bundle
+from flask_wtf import CsrfProtect
 
 from ocfnet.assets import js, babel, css
+from ocfnet.user import login_manager
+from ocfnet.user.forms import LoginForm, RegistrationForm
 
 
 app = Flask(__name__)
 
-# Assets: js, jsx, css
+try:
+    app.config.from_object('config')
+except:
+    app.config.from_object('configdist')
+
+app.secret_key = app.config['SECRET_KEY']
+
+csrf = CsrfProtect()
+csrf.init_app(app)
+
+# Initialize login manager
+login_manager.init_app(app)
+
+# Assets bundles: js, jsx, css
 assets = Environment(app)
 assets.register('js', js)
 assets.register('babel', babel)
