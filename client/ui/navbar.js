806e559bcd55cf249725dca71e759ac89e2e3085
spectralsun <garrett@spectralsun.com>
2015-08-19 14:23:24 -0700

    export navbar

--- client/ui/navbar.js
+++ client/ui/navbar.js
@@ -2,17 +2,34 @@ import React from 'react';
 import { Nav, Navbar, NavItem, DropdownButton, MenuItem } 
   from 'react-bootstrap';
 
-export const navbarInstance = (
-  <Navbar brand='OCF Intranet'>
-    <Nav>
-      <NavItem eventKey={1} href='/home'>Home</NavItem>
-      <NavItem eventKey={2} href='/media'>Media</NavItem>
-      
-    </Nav>
-    <Nav right>
-      <NavItem eventKey={1} href='/login'>Login</NavItem>
-    </Nav>
-  </Navbar>
-);
 
-React.render(navbarInstance, document.getElementById('nav'));
+var NavbarInstance = module.exports = React.createClass({
+  getInitialState() {
+    window.ui_navbar = this;
+    return window.APP_DATA;
+  },
+  render() { 
+    if (!this.state)
+      this.getInitialState();
+    var userNav = this.state.authed ? ( 
+      <Nav right>
+        <DropdownButton eventKey={3} title={<span>{this.state.username}</span>} right>
+            <MenuItem eventKey={1} href='/logout'>Logout</MenuItem>
+        </DropdownButton>
+      </Nav>
+    ) : (
+      <Nav right>
+        <NavItem eventKey={1} href='/login'>Login</NavItem>
+      </Nav>
+    )
+    return (
+      <Navbar brand='OCF Intranet' inverse>
+        <Nav>
+          <NavItem eventKey={1} href='/home'>Home</NavItem>
+          <NavItem eventKey={2} href='/media'>Media</NavItem>
+        </Nav>
+        {userNav}
+      </Navbar>
+    )
+  }
+})
