c6fdc7e9277f7a4ccd1a9d5e7d1728136d94d6d3
spectralsun <garrett@spectralsun.com>
2015-08-19 00:43:18 -0700

    login and logout routes

--- ocfnet/user/__init__.py
+++ ocfnet/user/__init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
-from flask.ext.login import LoginManager
+from flask.ext.login import LoginManager, login_user, logout_user, \
+    current_user
 
-from ocfnet.user.forms import RegistrationForm
+from ocfnet.user.forms import RegistrationForm, LoginForm
 from ocfnet.user.models import User
 from ocfnet.util import jsonify
 
@@ -19,3 +20,22 @@ def register():
         return ''
     form.errors['_status_code'] = 400 
     return jsonify(**form.errors)
+
+@user.route('/login', methods=['POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.get_user(form.username.data)
+        if user and user.check_password(form.password.data):
+            login_user(user)
+        else:
+            return jsonify(username=['Invalid username, email or password.'], 
+                _status_code=400)
+        return ''
+    form.errors['_status_code'] = 400 
+    return jsonify(**form.errors)
+
+@user.route('/logout', methods=['POST'])
+def logout():
+    logout_user()
+    return ''
