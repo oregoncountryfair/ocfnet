fc64b7bed64028ea6343e23dddb4bd1a559d004e
spectralsun <garrett@spectralsun.com>
2015-08-18 21:08:27 -0700

    basic assets

--- ocfnet/__init__.py
+++ ocfnet/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, session
-from flask.ext.assets import Environment, Bundle
+from flask.ext.assets import Environment
 from flask_wtf import CsrfProtect
 
-from ocfnet.assets import js, babel, css
+from ocfnet.assets import css
 from ocfnet.user import login_manager
 from ocfnet.user.forms import LoginForm, RegistrationForm
 
@@ -23,10 +23,12 @@ csrf.init_app(app)
 login_manager.init_app(app)
 
 # Assets bundles: js, jsx, css
-assets = Environment(app)
-assets.register('js', js)
-assets.register('babel', babel)
-assets.register('css', css)
+env = Environment(app)
+env.load_path = [
+    'node_modules',
+    'client/style'
+]
+env.register('css', css)
 
 @app.route('/<path:path>')
 def index(path):
