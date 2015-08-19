aa9c0ac263a8807490f07a94cce9e32b7eebbc02
spectralsun <garrett@spectralsun.com>
2015-08-19 00:26:07 -0700

    working registration form

--- ocfnet/user/forms.py
+++ ocfnet/user/forms.py
@@ -1,21 +1,32 @@
 from wtforms import validators, HiddenField, PasswordField, StringField, \
-    TextField, BooleanField
+    TextField, BooleanField, ValidationError
+from wtforms.fields.html5 import EmailField
 from wtforms.widgets import TextArea
-
 from flask_wtf import Form
+
+from ocfnet.user.models import User
 
 
 class RegistrationForm(Form):
     username = TextField('Username', [validators.Length(min=4, max=25)])
-    email = TextField('Email Address', [validators.Length(min=6, max=35)])
+    email = EmailField('Email Address', [validators.DataRequired(), validators.Email()])
     password = PasswordField('New Password', [
         validators.Required(),
+        validators.Length(min=8),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
     confirm = PasswordField('Repeat Password')
-    accept_tos = BooleanField('I agree to follow the OCF Intranet Rules', [
-        validators.Required()
-    ])
+    #accept_tos = BooleanField('I agree to follow the OCF Intranet Rules', [
+    #    validators.Required()
+    #])
+
+    def validate_username(form, field):
+        if User.get_user_by_username(field.data):
+            raise ValidationError('Username already registered.');
+
+    def validate_email(form, field):
+        if User.get_user_by_email(field.data):
+            raise ValidationError('Email already registered.')
 
 class LoginForm(Form):
     username = TextField('Username', validators=[validators.DataRequired()])
