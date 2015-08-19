8ca4cb21f5639b1a3b01b4376b76adcf8296fe15
spectralsun <garrett@spectralsun.com>
2015-08-18 21:03:20 -0700

    build_js manage command

--- manage.py
+++ manage.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-from flask.ext.script import Manager
+from subprocess import call
+
+from flask.ext.script import Manager, Command
 
 from ocfnet import app
 
 
 manager = Manager(app)
+
+@manager.command
+def build_js():
+    call(['webpack', './webpack.config.js'])
 
 if __name__ == '__main__':
     manager.run()
