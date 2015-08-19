e029f37985f69d330926cf0ee3cbd1c030b5c8cd
spectralsun <garrett@spectralsun.com>
2015-08-19 00:17:29 -0700

    register user blueprint

--- ocfnet/__init__.py
+++ ocfnet/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.assets import Environment
 from flask_wtf import CsrfProtect
 
 from ocfnet.assets import css
-from ocfnet.user import login_manager
+from ocfnet.user import login_manager, user
 from ocfnet.user.forms import LoginForm, RegistrationForm
 
 
@@ -15,6 +15,8 @@ except:
     app.config.from_object('configdist')
 
 app.secret_key = app.config['SECRET_KEY']
+
+app.register_blueprint(user)
 
 csrf = CsrfProtect()
 csrf.init_app(app)
