21c40fe9593fce5ec667ba71a8c27aeda9bf167f
spectralsun <garrett@spectralsun.com>
2015-09-05 19:53:00 -0700

    use props for user info

--- client/pages/HomePage.js
+++ client/pages/HomePage.js
@@ -3,6 +3,5 @@ import React from 'react';
 export default class HomePage extends React.Component
 {
     render() { 
-        var username = window.APP_DATA.authed ? window.APP_DATA.username : 'guest';
-        return (<div>Welcome, {username}.</div>) }
+        return (<div>Welcome, {this.props.user.username || 'guest'}.</div>) }
 }
